0372d359b7869d8a1a061a777f53535d8f8b6c4e
irfankhan <er.irfan2798@gmail.com>
2023-12-07 12:21:49 +0530

    new update

--- allFunction.js
+++ allFunction.js
@@ -1,199 +1,48 @@
-const pencil = document.getElementById("pencil");
-const pencilDiv = document.getElementById("pencilDiv");
-const ellipseButton = document.getElementById("elipsDiv");
-const squareBtn = document.getElementById("squareBtn");
-const linebtn = document.getElementById("lineBtn");
-const dimondBtn = document.getElementById("dimondBtn");
-const arrowLineBtn = document.getElementById("arrowLineBtn");
-const eraserBtn = document.getElementById("eraserBtn");
-const colorPicker = document.getElementById("color-picker");
+const undoStack = [];
+const redoStack = [];
 
-// colorPicker.addEventListener("change", () => {
+// Function to save the current canvas state to the undo stack
+function saveCanvasState() {
+    undoStack.push(canvas.toDataURL());
+    redoStack.length = 0; // Clear the redo stack
+}
 
-//     drawingColor = colorPicker.value ;
-//     c.strokeStyle = drawingColor;
-//     //console.log(drawingColor);
-// });
-
-// drawing line with pen
-let isPencilActive = false; // initially pencil is inactive
-pencilDiv.addEventListener("click", (e) => {
-  isElipsDrawing = false;
-  isDrawingLine = false;
-  isDrawingRectangle=false;
-  isDrawingDiamond=false;
-  isDrawingArrow=false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
+// Implement undo functionality
+const undoButton = document.getElementById('undo');
+undoButton.addEventListener('click', () => {
+    if (undoStack.length > 1) {
+        redoStack.push(undoStack.pop()); // Move the current state to the redo stack
+        const snapshot = new Image();
+        snapshot.src = undoStack[undoStack.length - 1];
+        snapshot.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(snapshot, 0, 0);
+        };
     }
-  }
-  pencilDiv.classList.toggle("active");
-
-  isPencilActive = !isPencilActive;
-
-  if (isPencilActive) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
 });
 
-// eclips drawing
-
-let isElipsDrawing = false;
-ellipseButton.addEventListener("click", (e) => {
-  isPencilActive = false;
-  isDrawingLine = false;
-  isDrawingRectangle=false;
-  isDrawingDiamond=false;
-  isDrawingArrow=false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
+// Implement redo functionality
+const redoButton = document.getElementById('redo');
+redoButton.addEventListener('click', () => {
+    if (redoStack.length > 0) {
+        const snapshot = new Image();
+        snapshot.src = redoStack.pop();
+        snapshot.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(snapshot, 0, 0);
+            undoStack.push(snapshot.src); // Move the current state back to the undo stack
+        };
     }
-  }
-  ellipseButton.classList.toggle("active");
-
-  isElipsDrawing = !isElipsDrawing;
-  if (isElipsDrawing) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
 });
 
-// Arrow Line drawing
-let isDrawingLine = false;
-linebtn.addEventListener("click", (e) => {
-  isPencilActive = false;
-  isElipsDrawing = false;
-  isDrawingRectangle=false;
-  isDrawingDiamond=false;
-  isDrawingArrow = false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
-    }
-  }
-  linebtn.classList.toggle("active");
+// Add a function to clear the undo and redo stacks
+function clearUndoRedoStacks() {
+    undoStack.length = 0;
+    redoStack.length = 0;
+}
 
-  isDrawingLine = !isDrawingLine;
-  if (isDrawingLine) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
-});
-
-let isDrawingRectangle = false;
-squareBtn.addEventListener("click", (e) => {
-  isPencilActive = false;
-  isDrawingLine = false;
-  isElipsDrawing=false;
-  isDrawingDiamond=false;
-  isDrawingArrow=false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
-    }
-  }
-  squareBtn.classList.toggle("active");
-
-  isDrawingRectangle = !isDrawingRectangle;
-  if (isDrawingRectangle) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
-});
-
-let isDrawingDiamond = false;
-dimondBtn.addEventListener("click", (e) => {
-  isPencilActive = false;
-  isDrawingLine = false;
-  isElipsDrawing=false;
-  isDrawingRectangle=false;
-  isDrawingArrow=false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
-    }
-  }
-  dimondBtn.classList.toggle("active");
-
-  isDrawingDiamond = !isDrawingDiamond;
-  if (isDrawingDiamond) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
-});
-
-
-let isDrawingArrow = false;
-arrowLineBtn.addEventListener("click", (e) => {
-  isPencilActive = false;
-  isDrawingLine = false;
-  isElipsDrawing=false;
-  isDrawingRectangle=false;
-  isDrawingDiamond=false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
-    }
-  }
-  arrowLineBtn.classList.toggle("active");
-
-  isDrawingArrow = !isDrawingArrow;
-  if (isDrawingArrow) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
-});
-
-
-let isEraser = false;
-eraserBtn.addEventListener("click", (e) => {
-  isPencilActive = false;
-  isDrawingLine = false;
-  isElipsDrawing=false;
-  isDrawingRectangle=false;
-  isDrawingDiamond=false;
-  isDrawingArrow = false;
-  let activeButton = document.getElementsByClassName("active");
-  if (activeButton.length > 0 && activeButton[0] != e.currentTarget) {
-    for (let i = 0; i < activeButton.length; i++) {
-      activeButton[i].classList.remove("active");
-    }
-  }
-  eraserBtn.classList.toggle("active");
-
-  isEraser = !isEraser;
-  if (isEraser) {
-    canvas.style.cursor = "crosshair";
-    canvas.addEventListener("mousedown", onMouseDown);
-  } else {
-    canvas.style.cursor = "auto";
-    canvas.removeEventListener("mousedown", onMouseDown);
-  }
+// Call saveCanvasState when drawing is done
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+    saveCanvasState();
 });
